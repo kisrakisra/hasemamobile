01f9a471bb23192df1e5f57542b397aded0589ca
kisrakisra <kisrasumeyye@gmail.com>
2015-07-27 18:01:55 +0300

    Listview dynamic height and fixed header added

--- myapplication/src/main/java/vizyonmedya/myapplication/MainActivity.java
+++ myapplication/src/main/java/vizyonmedya/myapplication/MainActivity.java
@@ -163,13 +163,37 @@ public class MainActivity extends Activity {
                     liste.setAdapter(lazyadapter);
 
 
+
                 } catch (Exception exc) {
                 }
 
             }
 
         });
+        /**** Method for Setting the Height of the ListView dynamically.
+         **** Hack to fix the issue of not showing all the items of the ListView
+         **** when placed inside a ScrollView  ****/
+        public  void setListViewHeightBasedOnChildren(ListView listView) {
+            ListAdapter listAdapter = listView.getAdapter();
+            if (listAdapter == null)
+                return;
 
+            int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+            int totalHeight = 0;
+            View view = null;
+            for (int i = 0; i < listAdapter.getCount(); i++) {
+                view = listAdapter.getView(i, view, listView);
+                if (i == 0)
+                    view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+                totalHeight += view.getMeasuredHeight();
+            }
+            ViewGroup.LayoutParams params = listView.getLayoutParams();
+            params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+            listView.setLayoutParams(params);
+            listView.requestLayout();
+        }
         @Override
         protected void onPreExecute() {
             threads[0] = thread1;
@@ -190,6 +214,7 @@ public class MainActivity extends Activity {
                 threads[0].join();
                 threads[1].start();
                 threads[1].join();
+                setListViewHeightBasedOnChildren(liste);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -206,7 +231,6 @@ public class MainActivity extends Activity {
         logoMoveAnimation.setFillAfter(true);
         logoMoveAnimation.setFillEnabled(true);
         imageLogo.clearAnimation();
-        
         imageLogo.startAnimation(logoMoveAnimation);
     }
 
